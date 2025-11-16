861d5823427b193b2cc274a4ff95237d8f8d4907
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-11-16 16:48:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+namespace Gestón_de_Libros
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Si quieres inyectar repo como singleton:
+            // builder.Services.AddSingleton<LibroRepository>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Libro}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
